76a6a9c21297accf382e79307df0b81f79827701
Brett Lyons <blyons@students.naropa.edu>
2016-01-26 16:08:36 -0700

    Finished exercises, but not bonuses

--- code/part1_exercises/exercises/compose/compose_exercises.js
+++ code/part1_exercises/exercises/compose/compose_exercises.js
@@ -45,8 +45,7 @@ var averageDollarValue = _.compose(_average, _.map(_.prop('dollar_value')));
 
 var _underscore = replace(/\W+/g, '_'); //<-- leave this alone and use to sanitize
 
-var sanitizeNames = undefined;
-
+var sanitizeNames = _.compose(_.compose(_.map(toLowerCase), _.map(_underscore)), _.map(_.prop('name')));
 
 // Bonus 1:
 // ============
